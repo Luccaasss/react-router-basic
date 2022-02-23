084d80cec36e7cc338aceb3852be37fa6b20e76a
Luccaasss <lucasrodriguesprb@gmail.com>
2022-02-22 23:04:18 -0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { Outlet, Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+      <nav>
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link> 
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
